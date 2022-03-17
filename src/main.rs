cfbec1a04574d2375abfe45a2d1a6a8813618441
Bartłomiej Osiński <bartlomiej.osinski@enigma.com.pl>
2022-03-18 00:36:02 +0100

    MVP
    Audio playing on valid email
    
    More robustness on connection loss/change
    
    Remove unused parameter
    
    get_date()
    
    After login read mails first, then IDLE
    
    Missing header
    
    mail_too_old()
    
    Do not play audio for older emails, just move them
    
    Removed fixed TODOs
    
    Extra feature: accept typos in topic
    
    Let's be more fp
    
    fix referencing wrong email uids after deletion
    
    Handle SSL failures
    
    Don't forget to reap zombies
    
    Reading allowed people list from json file
    
    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use structopt::StructOpt;
 use email::rfc2047::decode_rfc2047;
+use email::FromHeader;
+use std::io::{Read, Write, BufReader};
+use std::thread;
+use std::process::Command;
+use std::fs::File;
+use std::error::Error;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "idle")]
@@ -27,13 +33,25 @@ struct Opt {
     #[structopt(short, long, default_value = "INBOX")]
     mailbox: String,
 
-    #[structopt(
-        short = "x",
-        long,
-        help = "The number of responses to receive before exiting",
-        default_value = "5"
-        )]
-        max_responses: usize,
+    // Refresh rate in seconds
+    #[structopt(short, long, default_value = "10")]
+    refresh_rate: u64,
+
+    // Json list of allowed people
+    #[structopt(long, default_value = "allowed_people.json")]
+    allowed_people: String,
+
+    // Json list of subjects that trigger audio
+    #[structopt(long, default_value = "triggering_subjects.json")]
+    triggering_subjects: String,
+
+    // When is mail considered too old?
+    #[structopt(short = "e", long, default_value = "180")]
+    mail_expiration_secs: u32,
+
+    // Audio file to play
+    #[structopt(short, long, default_value = "~/Music/kanapkiv2.wav")]
+    audio_file: String,
 }
 
 fn get_subject(header: &imap::types::Fetch<'_>) -> String {
@@ -48,6 +66,19 @@ fn get_subject(header: &imap::types::Fetch<'_>) -> String {
     }
 }
 
+fn get_date(header: &imap::types::Fetch<'_>) -> chrono::DateTime<chrono::Utc> {
+    let envelope = header.envelope().unwrap();
+
+    let _date = envelope.date.as_ref().unwrap();
+    let date = String::from_utf8_lossy(_date);
+    let date = if let Some(decoded_date) = decode_rfc2047(&date) {
+        decoded_date
+    } else {
+        date.to_string()
+    };
+    FromHeader::from_header(date).unwrap()
+}
+
 fn get_from(header: &imap::types::Fetch<'_>) -> String {
     let envelope = header.envelope().unwrap();
 
@@ -60,58 +91,137 @@ fn get_from(header: &imap::types::Fetch<'_>) -> String {
     }
 }
 
+fn person_allowed(person: &String, allowed_people: &Vec<String>) -> bool {
+    allowed_people.contains(person)
+}
+
+fn parse_json_to_vector(filepath: &String) -> Result<Vec<String>, Box<dyn Error>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let vector = serde_json::from_reader(reader)?;
+
+    Ok(vector)
+}
+
+fn subject_is_triggering(subject: &str, allowed_subjects: &Vec<String>) -> bool {
+    let subject = subject.to_lowercase();
+    allowed_subjects.iter().any(|allowed_subject| edit_distance::edit_distance(&subject, allowed_subject) <= 2)
+}
+
+fn mail_too_old(date: chrono::DateTime<chrono::Utc>, limit_secs: u32) -> bool {
+    let now_date = chrono::offset::Utc::now();
+    let difference_in_seconds = (now_date - date).num_seconds() as u32;
+    difference_in_seconds > limit_secs
+}
+
+fn move_email<T: Read + Write>(imap: &mut imap::Session<T>, mail_uid: u32, target_folder: &str) {
+    imap.copy(mail_uid.to_string(), target_folder).unwrap();
+    imap.store(mail_uid.to_string(), "+FLAGS (\\Deleted)").unwrap();
+    imap.expunge().unwrap();
+}
+
+fn play_notification_sound() {
+    thread::spawn(|| {
+        let opt = Opt::from_args();
+        if let Ok(mut child) = Command::new("play").arg(opt.audio_file).spawn() {
+            child.wait().expect("Command wasn't running");
+        } else {
+            println!("Failed to run command");
+        }
+    });
+}
+
 fn main() {
-    let opt = Opt::from_args();
+    'connect: loop {
+        let opt = Opt::from_args();
 
-    let client = imap::ClientBuilder::new(opt.server.clone(), opt.port)
-        .native_tls()
-        .expect("Could not connect to imap server");
+        println!("Trying to log in to mailbox");
 
-    let mut imap = client
-        .login(opt.username, opt.password)
-        .expect("Could not authenticate");
+        let client = match imap::ClientBuilder::new(opt.server.clone(), opt.port).native_tls() {
+            Ok(client) => client,
+            Err(e) => {
+                let dur: std::time::Duration = std::time::Duration::from_secs(opt.refresh_rate);
+                println!("Failed to create ClientBuilder: {e:?}");
+                println!("Waiting {}s to reconnect", &opt.refresh_rate);
+                std::thread::sleep(dur);
+                continue 'connect;
+            },
+        };
 
-    // Turn on debug output so we can see the actual traffic coming
-    // from the server and how it is handled in our callback.
-    // This wouldn't be turned on in a production build, but is helpful
-    // in examples and for debugging.
-    imap.debug = false;
+        let mut imap: imap::Session<_> = match client.login(opt.username, opt.password) {
+            Ok(imap) => imap,
+            Err(e) => {
+                let dur: std::time::Duration = std::time::Duration::from_secs(opt.refresh_rate);
+                println!("Failed to login: {e:?}");
+                println!("Waiting {}s to reconnect", &opt.refresh_rate);
+                std::thread::sleep(dur);
+                continue 'connect;
+            },
+        };
 
-    imap.select(opt.mailbox).expect("Could not select mailbox");
+        // Turn on debug output so we can see the actual traffic coming
+        // from the server and how it is handled in our callback.
+        // This wouldn't be turned on in a production build, but is helpful
+        // in examples and for debugging.
+        imap.debug = false;
 
-    loop {
-        let idle_result = imap.idle().wait_while(|_response| {
-            false
-            //if let imap::types::UnsolicitedResponse::Recent(no) = response {
-            //    println!("Recorded {no} new mails");
-            //    false
-            //} else {
-            //    true
-            //}
-        });
+        imap.select(opt.mailbox).expect("Could not select mailbox");
 
-        match idle_result {
-            Ok(reason) => println!("IDLE finished normally {reason:?}"),
-            Err(e) => println!("IDLE finished with error {e:?}"),
-        }
+        'fetch_mails: loop {
 
-        let search_results = imap.search("UNSEEN").unwrap();
-        println!("Search results: {:?}", &search_results);
-        for result in search_results.iter() {
-            println!("Parsing email of UID {result}");
-            let messages = imap.fetch(result.to_string(), "ENVELOPE").unwrap();
-            if let Some(header) = messages.iter().next() {
-                let subject = get_subject(header);
-                println!("Subject is \"{subject}\"");
+            let search_results = match imap.search("UNSEEN") {
+                Ok(search_results) => {
+                    println!("Search results: {:?}", &search_results);
+                    search_results
+                },
+                Err(e) => {
+                    println!("Failed to fetch emails: {e:?}");
+                    continue 'connect;
+                }
+            };
 
-                let from = get_from(header);
-                println!("Subject is \"{from}\"");
-            } else {
-                println!("Header not found :(");
+            for mail_uid in search_results.iter() {
+                println!("Parsing email of UID {mail_uid}");
+                let messages = imap.fetch(mail_uid.to_string(), "ENVELOPE").unwrap();
+                if let Some(header) = messages.iter().next() {
+                    let date = get_date(header);
+                    let from = get_from(header);
+                    let subject = get_subject(header);
+                    let allowed_people = parse_json_to_vector(&opt.allowed_people)
+                        .expect(format!("Failed to get {}", opt.allowed_people).as_ref());
+                    let triggering_subjects = parse_json_to_vector(&opt.triggering_subjects)
+                        .expect(format!("Failed to get {}", opt.triggering_subjects).as_ref());
+                    if person_allowed(&from, &allowed_people) && subject_is_triggering(&subject, &triggering_subjects) {
+                        move_email(&mut imap, *mail_uid, "Jedzenie");
+                        if !mail_too_old(date, opt.mail_expiration_secs) {
+                            println!("New mail from {from}: \"{subject}\"");
+                            play_notification_sound();
+                        }
+                        continue 'fetch_mails; // search for mails again, because mail uid's are at this point invalid
+                    }
+                } else {
+                    println!("Header not found :(");
+                }
             }
+
+            println!("Waiting for something to arrive");
+
+            let dur: std::time::Duration = std::time::Duration::from_secs(opt.refresh_rate);
+
+            let idle_result = imap.idle().timeout(dur).wait_while(|_response| {
+                false
+            });
+
+            match idle_result {
+                Ok(reason) => println!("IDLE finished normally {reason:?}"),
+                Err(e) => {
+                    println!("IDLE finished with error: {e:?}");
+                    continue 'connect;
+                }
+            }
+
         }
-
+        //imap.logout().expect("Could not log out");
     }
-
-    //imap.logout().expect("Could not log out");
 }
